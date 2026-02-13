366f17be9cfe46972435afdce018b6a27742bd0f
Thong Nguyen <nguyentom1996@yahoo.com>
2026-02-13 15:33:11 -0800

    Add script
    add script

---
+++ Public/js/main.js
@@ -0,0 +1,68 @@
+const answer_no = [        
+        "No",
+        "Are you sure?",
+        "Are you really sure??",
+        "Are you really realy sure???",
+        "Think again?",
+        "Don't believe in second chances?",
+        "Why are you being so cold?",
+        "Maybe we can talk about it?",
+        "I am not going to ask again!",
+        "Ok now this is hurting my feelings!",
+        "You are now just being mean!",
+        "Why are you doing this to me?",
+        "Please give me a chance!",
+        "I am begging you to stop!",
+        "Ok, Let's just start over.."];
+
+const answer_yes = "Yes";
+
+
+const no_button = document.getElementById("no-button");
+const yes_button = document.getElementById("yes-button");
+
+let i  =  1;
+let size = 50;
+let clicks = 0;
+
+no_button.addEventListener('click', () => {
+
+    let banner = document.getElementById("banner");
+    if(clicks == 0){
+        banner.src = "public/images/no.gif";
+        refreshBanner();
+    }
+
+
+    clicks++;
+
+    const sizes = [40, 50, 30, 35, 45]
+    const random = Math.floor(Math.random() * sizes.length);
+    size += sizes[random]
+    yes_button.style.height = `${size}px`;
+    yes_button.style.width = `${size}px`;
+    let total = answer_no.length;
+    // change button text
+    if (i < total - 1) {
+        no_button.innerHTML = answer_no[i];
+        i++;
+    } else if (i === total - 1) {
+        alert(answer_no[i]);
+        i = 1;
+        no_button.innerHTML = answer_no[0];
+        yes_button.innerHTML = answer_yes;
+        yes_button.style.height = "50px";
+        yes_button.style.width = "50px";
+        size = 50;
+    }
+
+
+});
+
+
+function refreshBanner(){
+    let banner = document.getElementById("banner");
+    let src = banner.src;
+    banner.src = "";
+    banner.src = src;
+}
